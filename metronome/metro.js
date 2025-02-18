04f4e832e539789233bad0b55f6cb38d66c81999
natebooth2026 <nbooth5@kent.edu>
2025-02-18 13:16:53 -0500

    Instantiate metro.js

--- metronome/metro.js
+++ metronome/metro.js
@@ -1,91 +1,103 @@
-let metroSound;
-let metroGraphic;
-let canvas;
-let inputBPM;
-let metroPlay;
-let showBPM;
+let metroSketch = function(p) {
+  let metroSound;
+  let metroGraphic;
+  let canvas;
+  let inputBPM;
+  let metroPlay;
+  let showBPM;
 
-const SIZE_X = 50;
-const SIZE_Y = 50;
+  const SIZE_X = 50;
+  const SIZE_Y = 50;
 
-let timeouts = [];
+  let timeouts = [];
 
-function preload(){
-  metroSound = loadSound("https://src-img.github.io/deltaTeam/metronome/metro.wav");
-  metroGraphic = loadImage("https://src-img.github.io/deltaTeam/metronome/metroLogo.png");
-}
+  p.preload = function() {
+      metroSound = p.loadSound("https://src-img.github.io/deltaTeam/metronome/metro.wav");
+      metroGraphic = p.loadImage("https://src-img.github.io/deltaTeam/metronome/metroLogo.png");
+  };
 
-function setup() {
-  canvas = createCanvas(SIZE_X, SIZE_Y);
-  canvas.id("metroImg");
-  
-  image(metroGraphic, 0, 0, SIZE_X, SIZE_Y); //applied to canvas element
-  
-  inputBPM = createSlider(1, 218);
-  inputBPM.position(50, 10);
-  inputBPM.size(100);
-  inputBPM.input(inputHandler);
-  inputBPM.id("metroSlider");
-  
-  metroPlay = createButton("Play");
-  metroPlay.mousePressed(toggle);
-  metroPlay.position(190, 10);
-  metroPlay.id("metroPlay");
-  
-  showSlider = createP("60");
-  showSlider.position(155, -5);
-  showSlider.id("metroSliderCount");
-  
-  showBPM = createP("60 BPM");
-  showBPM.position(50, 20);
-  showBPM.id("metroBPM");
-}
+  p.setup = function() {
+      let div = document.getElementById("metroContainer");
 
-function inputHandler(){
-  metroSound.stop();
-  
-  for(let i = 0; i < timeouts.length; i++){
-    clearTimeout(timeouts[i]);
+      canvas = p.createCanvas(SIZE_X, SIZE_Y);
+      canvas.id("metroImg");
+      canvas.parent(div);
+
+      p.image(metroGraphic, 0, 0, SIZE_X, SIZE_Y); // Applied to canvas element
+
+      inputBPM = p.createSlider(1, 218);
+      inputBPM.position(50, 280);
+      inputBPM.size(100);
+      inputBPM.input(inputHandler);
+      inputBPM.id("metroSlider");
+      inputBPM.parent(div);
+
+      metroPlay = p.createButton("Play");
+      metroPlay.mousePressed(toggle);
+      metroPlay.position(185, 270);
+      metroPlay.id("metroPlay");
+      metroPlay.parent(div);
+
+      showSlider = p.createP("60");
+      showSlider.position(153, 265);
+      showSlider.id("metroSliderCount");
+      showSlider.parent(div);
+
+      showBPM = p.createP("60 BPM");
+      showBPM.position(50, 285);
+      showBPM.id("metroBPM");
+      showBPM.parent(div);
+  };
+
+  function inputHandler() {
+      metroSound.stop();
+
+      for (let i = 0; i < timeouts.length; i++) {
+          clearTimeout(timeouts[i]);
+      }
+
+      timeouts.splice(0, timeouts.length);
+
+      showSlider.html(inputBPM.value());
+      showBPM.html("Changing BPM");
+
+      timeouts.push(setTimeout(changeBPM, 750));
   }
-  
-  timeouts.splice(0, timeouts.length);
-  
-  showSlider.html(inputBPM.value());
-  showBPM.html("Changing BPM");
-  
-  timeouts.push(setTimeout(changeBPM, 750));
-}
 
-function changeBPM(){
-  showBPM.html(inputBPM.value() + " BPM");
-  
-  if(metroPlay.html() == "Pause"){
-    play(inputBPM.value());
-  } 
-}
+  function changeBPM() {
+      showBPM.html(inputBPM.value() + " BPM");
 
-function toggle(){
-  if(metroPlay.html() == "Play"){
-    metroPlay.html("Pause");
-  } else {
-    metroPlay.html("Play");
+      if (metroPlay.html() == "Pause") {
+          play(inputBPM.value());
+      }
   }
-  
-  play(inputBPM.value());
-}
 
-function play(BPM){  
-  if(metroPlay.html() == "Pause" && showBPM.html() != "Changing BPM"){
-    metroSound.play();
-    timeoutID = setTimeout(() => play(BPM), 60000/BPM);
-    timeouts.push(timeoutID);
-  } else {
-    clearTimeout(timeoutID);
-    metroSound.stop();
+  function toggle() {
+      if (metroPlay.html() == "Play") {
+          metroPlay.html("Pause");
+      } else {
+          metroPlay.html("Play");
+      }
+
+      play(inputBPM.value());
   }
-  
-  if(BPM != inputBPM.value()){
-    clearTimeout(timeoutID);
-    metroSound.stop();
+
+  function play(BPM) {
+      if (metroPlay.html() == "Pause" && showBPM.html() != "Changing BPM") {
+          metroSound.play();
+          timeoutID = setTimeout(() => play(BPM), 60000 / BPM);
+          timeouts.push(timeoutID);
+      } else {
+          clearTimeout(timeoutID);
+          metroSound.stop();
+      }
+
+      if (BPM != inputBPM.value()) {
+          clearTimeout(timeoutID);
+          metroSound.stop();
+      }
   }
-}
+};
+
+// Attach this sketch to the "metroContainer" div
+new p5(metroSketch, 'metroContainer');

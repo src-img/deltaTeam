981c3e31fb570bac3691936a672930ee140765d0
Josh <jshprnc@outlook.com>
2025-02-19 14:48:08 -0500

    dkd

--- static/metronome/metro.js
+++ static/metronome/metro.js
@@ -11,8 +11,8 @@ const SIZE_Y = 50;
 let timeouts = [];
 
 function preload(){
-  metroSound = loadSound("https://src-img.github.io/deltaTeam/metronome/metro.wav");
-  metroGraphic = loadImage("https://src-img.github.io/deltaTeam/metronome/metroLogo.png");
+  metroSound = loadSound("static/metronome/metro.wav");
+  metroGraphic = loadImage("static/metronome/metroLogo.png");
 }
 
 function setup() {

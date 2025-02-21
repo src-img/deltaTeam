1f88ef6b9a222486624f322c0b9390b750d1ffc2
joshprince2 <jshprnc@outlook.com>
2025-02-21 12:33:28 -0500

    .wav

--- static/metronome/metro.js
+++ static/metronome/metro.js
@@ -14,7 +14,7 @@ let metroSketch = function(p) {
   let timeouts = [];
 
   p.preload = function() {
-      metroSound = p.loadSound("https://src-img.github.io/deltaTeam//static/metronome/metro.js");
+      metroSound = p.loadSound("https://src-img.github.io/deltaTeam//static/metronome/metro.wav");
       metroGraphic = p.loadImage("https://src-img.github.io/deltaTeam//static/metronome/metroLogo.png");
   };
 

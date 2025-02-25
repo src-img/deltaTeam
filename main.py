f5ea6876aaf6ac6530b9eba45f7b2f16534ac179
WaddlesPF <113570007+WaddlesPF@users.noreply.github.com>
2025-02-25 13:26:29 -0500

    data handler and flask together

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, redirect, url_for
 from recordUserInput import Composition, modifyComposition, InputState
+from database.DBHandler import databaseManager
 
 app = Flask(__name__)
+db = databaseManager("testDB")
+success, error = db.connect()
+
 
 @app.route("/")
 def index():
@@ -19,9 +23,44 @@ def faq():
 def login():
     return render_template('login.html')
 
+@app.route('/login', methods=['POST'])
+def login_submit():
+    email = request.form['username']
+    password = request.form['password']
+    
+    result, error = db.fetchUser(email)
+    if result != None:
+        if (result[1] == email) and (result[3] == password):
+            print("logged in as " + result[2])
+        else:
+            return redirect(url_for('login'))
+    else:
+        print("user does not exist")
+        return redirect(url_for('login'))
+    
+    return redirect(url_for('index'))
+
 @app.route("/features")
 def features():
     return render_template('features.html')
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+
+@app.route('/signup', methods=['POST'])
+def signup_submit():
+    username = request.form['username']
+    email = request.form['email']
+    password = request.form['password']
+    confirm_password = request.form['confirm_password']
+    
+    success, error, user_id = db.addUser(email, username, password)
+    db.commit()
+    print(error)
+    return redirect(url_for('login'))
+
 
 temp = Composition()
 

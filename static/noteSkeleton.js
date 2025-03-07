4fc99bbf549ecbacfa8a81460cf7345ad5a016a3
natebooth2026 <nbooth5@kent.edu>
2025-03-07 15:28:44 +0000

    fixed internal ID issue & organizational stuff

--- static/noteSkeleton.js
+++ static/noteSkeleton.js
@@ -93,8 +93,69 @@ let skeletonSketch = function(p) {
           let trackNums = document.getElementsByClassName("numberDisplay");
           for (let i = 0; i < trackNums.length; i++) {
             trackNums[i].innerHTML = i + 1;
+            trackNums[i].id = "numberDisplay" + i;
           }
-          holderCount--;
+
+          let trackTracks = document.getElementsByClassName("trackContainer");
+          for (let i = 0; i < trackTracks.length; i++) {
+            trackTracks[i].id = "trackContainer" + i;
+          }
+
+          let trackButton = document.getElementsByClassName("buttonContainer");
+          for (let i = 0; i < trackButton.length; i++) {
+            trackButton[i].id = "buttonContainer" + i;
+          }
+
+          let trackButtonRA = document.getElementsByClassName("buttonContainerRowA buttonContainerRow");
+          for (let i = 0; i < trackButtonRA.length; i++) {
+            trackButtonRA[i].id = "buttonContainerRowA" + i;
+          }
+
+          let trackButtonRB = document.getElementsByClassName("buttonContainerRowB buttonContainerRow");
+          for (let i = 0; i < trackButtonRB.length; i++) {
+            trackButtonRB[i].id = "buttonContainerRowB" + i;
+          }
+
+          let trackButtonRC = document.getElementsByClassName("buttonContainerRowC buttonContainerRow");
+          for (let i = 0; i < trackButtonRC.length; i++) {
+            trackButtonRC[i].id = "buttonContainerRowC" + i;
+          }
+
+          let trackName = document.getElementsByClassName("trackName");
+          for (let i = 0; i < trackName.length; i++) {
+            trackName[i].id = "trackName" + i;
+          }
+
+          let trackRecord = document.getElementsByClassName("trackRecord");
+          for (let i = 0; i < trackRecord.length; i++) {
+            trackRecord[i].id = "trackRecord" + i;
+          }
+
+          let trackMute = document.getElementsByClassName("trackMute");
+          for (let i = 0; i < trackMute.length; i++) {
+            trackMute[i].id = "trackMute" + i;
+          }
+
+          let trackIso = document.getElementsByClassName("trackIso");
+          for (let i = 0; i < trackIso.length; i++) {
+            trackIso[i].id = "trackIso" + i;
+          }
+
+          let trackDelete = document.getElementsByClassName("trackDelete");
+          for (let i = 0; i < trackDelete.length; i++) {
+            trackDelete[i].id = "trackDelete" + i;
+          }
+
+          let trackVolume = document.getElementsByClassName("trackVolume");
+          for (let i = 0; i < trackVolume.length; i++) {
+            trackVolume[i].id = "trackVolume" + i;
+          }
+
+          let trackNote = document.getElementsByClassName("noteContainer");
+          for (let i = 0; i < trackNote.length; i++) {
+            trackNote[i].id = "noteContainer" + i;
+          }
+
         }
       });
 
@@ -118,7 +179,7 @@ let skeletonSketch = function(p) {
       //NOTE CREATION ------------------------------------------------------------------------------
 
       this.noteContainer = this.p.createDiv();
-      this.noteContainer.id("noteContainer" + holderCount)
+      this.noteContainer.id("noteContainer" + holderCount);
       this.noteContainer.class("noteContainer");
       this.noteContainer.parent(this.trackContainer);
 
@@ -157,17 +218,11 @@ let skeletonSketch = function(p) {
   }
 
   function removeTrack(obj) {
-    let trackNums = document.getElementsByClassName("numberDisplay");
-    if(obj.number < holderCount){
-      for(let i = 0; i < trackNums.length; i++){
-        if(trackNums[i] > obj.number){
-          trackNums[i]
-          trackNums[i].id("numberDisplay" + i);
-        }
-      } 
-    }
+    tracks.splice(obj.id, 1); //removes track from array
+    current--; //for add track to add past an actual track
     obj.number.remove();
     obj.trackContainer.remove();
+    holderCount--;
   }
 
   p.setup = function(){

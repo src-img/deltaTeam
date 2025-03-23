4cfad3f5e4539832dda93295d6c11577c553fbcb
Josh <jshprnc@outlook.com>
2025-03-23 01:13:37 -0400

    rename userinput.js to eventListener.js add event_listener from index.html

---
+++ static/eventListeners.js
@@ -0,0 +1,27 @@
+
+
+
+
+document.addEventListener('keydown', function(event) {
+    const key = event.key; 
+    fetch('/keyboard_event', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ key: key })
+    })
+    .then(response => response.text())
+    .then(data => console.log(data));
+});
+
+
+window.onload = function() {
+    const composition_div = document.getElementById("composition")
+    document.addEventListener('handlePostEvent', function(event) {
+        event.preventDefault();
+        fetch("/compositionString")
+            .then(response => {return response.text();})
+            .then((html) => composition_div.innerHTML = html)          
+    })
+}   

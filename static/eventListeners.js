93188c8691afd2a8fc6a1ee646bd806030f4e6e5
Josh <jshprnc@outlook.com>
2025-03-30 01:22:59 -0400

    cleanup eventlistener.js

--- static/eventListeners.js
+++ static/eventListeners.js
@@ -33,15 +33,3 @@ document.addEventListener('togglePlay', (e) => {
     document.dispatchEvent(togglePlayback);
 })
 
-
-//  // Event listener to turn recording on/off
-//  const rButton = document.getElementById("trackRecord" + holderCount);
-//  rButton.addEventListener('click', () => {
-//    console.log("record button clicked!");
-
-//    fetch('/recording', {
-//      method: 'POST',
-//      headers: {'Content-Type': 'application/json'},
-//      body: JSON.stringify({key: 'value'})
-//    })
-//  })

3bc5c41976c120f26c682fb9f46e84d366a0031a
Josh <jshprnc@outlook.com>
2025-03-31 12:14:27 -0400

    clean up

--- static/noteSkeleton.js
+++ static/noteSkeleton.js
@@ -55,23 +55,6 @@ let skeletonSketch = function(p) {
       this.recordButton.parent(this.buttonContainerRowA);
 
       // Event listener to turn recording on/off
-      // const rButton = document.getElementById("trackRecord" + holderCount);
-      // rButton.addEventListener('click', () => {
-      //   console.log("record button clicked!");
-
-      //   let string = rButton.id;
-      //   console.log(string);
-      //   string = string.replace(/\D/g, ""); //gets only digits
-      //   console.log(string);
-        
-      //   fetch('/recording', {
-      //     method: 'POST',
-      //     headers: {'Content-Type': 'application/json'},
-      //     body: JSON.stringify({key: 'value', trackId: string})
-      //   })
-      // })
-
-      // Event listener to turn recording on/off
       const rButton = document.getElementById("trackRecord" + holderCount);
       rButton.addEventListener('click', () => {
       console.log("record button clicked!");
@@ -93,22 +76,6 @@ let skeletonSketch = function(p) {
       const toggleMetroEvent = new CustomEvent('toggleAction', {detail:{}});
       document.dispatchEvent(toggleMetroEvent);
       })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
       this.buttonContainerRowB = this.p.createDiv();

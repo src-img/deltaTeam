146b75d0aa50c03f29dbfa2c5d70aee9d592d48f
Josh <jshprnc@outlook.com>
2025-04-26 00:06:15 -0400

    clean up recordUserInput add loadComposition() and modify getCompMeasureList()

--- recordUserInput.py
+++ recordUserInput.py
@@ -9,63 +9,13 @@ class InputState(Enum):
 
 noteArray = ["s","e","i","q","qUs","j","jUs","h","hUs","hUe","hUi","d","dUs","dUe","dui","w"]
 restArray = ["S","E","I","Q","QS","J","JS","H","HS","HE","HI","D","DS","DE","DI","W"]
-emptyArray = ["","","","","","","","","","","","","","","",""]
-empty_comp = {'composition': 'g$|', 'sixteenth': 0, 'noteSizeLimit': 15, 'noteSize': 0, 'arrayPtr': emptyArray.copy()}
+emptyArray = ["",""]
+empty_comp = {'composition': 'g$|', 'sixteenth': 0, 'noteSizeLimit': 15, 'noteSize': 0, 'arrayPtr': emptyArray}
 
 class Composition:
-    # def __init__(self, comp = "g$|"):
-    #     self.composition = comp
-    #     self.sixteenth = self.setSixteenth(comp)
-    #     self.noteSizeLimit = self.setNoteSizeLimitInit(self.sixteenth)
-    #     self.noteSize = 0
-    #     self.arrayPtr = emptyArray.copy()
-    
-
-    # got rid of global instance of Composition - using session dictionary to intialize temporary instances of Compoosition class
-
     def __init__(self, empty_comp):
         for key, value in empty_comp.items():
             setattr(self, key, value)
-
-    def setSixteenth(self, composition):
-        sixteenth = 1
-        sixteenth += composition.count('s')
-        sixteenth += composition.count('S')
-        sixteenth += 2 * composition.count('e')
-        sixteenth += 2 * composition.count('E')
-        sixteenth += 3 * composition.count('i')
-        sixteenth += 3 * composition.count('I')
-        sixteenth += 4 * composition.count('q')
-        sixteenth += 4 * composition.count('Q')
-        sixteenth += 6 * composition.count('j')
-        sixteenth += 6 * composition.count('J')
-        sixteenth += 8 * composition.count('h')
-        sixteenth += 8 * composition.count('H')
-        sixteenth += 12 * composition.count('d')
-        sixteenth += 12 * composition.count('D')
-        sixteenth += 16 * composition.count('w')
-        sixteenth += 16 * composition.count('W')
-        sixteenth += 4 * composition.count('o')
-        sixteenth += 4 * composition.count('O')
-        sixteenth += 4 * composition.count('y')
-        sixteenth += 4 * composition.count('n')
-        sixteenth += 4 * composition.count('N')
-        sixteenth += 4 * composition.count('m')
-        sixteenth += 4 * composition.count('M')
-        sixteenth = sixteenth % 16
-        return sixteenth
-
-    def setNoteSizeLimitInit(self, sixteenth):
-        match ((sixteenth-1) % 4):
-            case 0: 
-                noteSizeLimit = 15 - ((sixteenth-1) % 16)
-            case 1:
-                noteSizeLimit = 1
-            case 2:
-                noteSizeLimit = 1
-            case 3:
-                noteSizeLimit = 0
-        return noteSizeLimit
 
     def setNoteSizeLimit(self):
         match (self.sixteenth % 4):
@@ -119,29 +69,35 @@ class Composition:
     
     def getCompMeasureList(self):
         result = []
+        comp = self.composition.replace("v", "U|" ) # handles "v" case
         delimiter = '|'
 
         # This is to check if it is closed
-        if self.composition.count(delimiter) == 1:
+        if comp.count(delimiter) == 1:
             return None
 
-        parts = self.composition.split(delimiter)    
+        parts = comp.split(delimiter)
         substrings = [parts[i] for i in range(1, len(parts) - 1)]
     
         return substrings
+
+    def loadComposition(self, measureList):
+        result = "g$|"
+        for measure in measureList.values():
+            if measure[-1] == "U": # if end of measure is 'U' then it deletes it and adds 'v'
+                measure = measure[:-1]
+                result += measure
+                result += 'v'
+            else:   
+                result += measure
+                result += '|'
+        self.composition = result
         
     def getComposition(self):
             return self.composition
     
     def getFutureNote(self):
         return self.arrayPtr[self.noteSize]
-
-    def deleteComposition(self):
-        self.composition = "g$|"
-        self.sixteenth = self.setSixteenth(self.composition)
-        self.noteSizeLimit = self.setNoteSizeLimitInit(self.sixteenth)
-        self.noteSize = 0
-        self.arrayPtr = emptyArray.copy()
 
     def parseComposition(self):
         self.composition = self.composition.replace("ius", "o")
@@ -158,6 +114,3 @@ class Composition:
     def to_dict(self):
         return self.__dict__.copy()
 
-# def __init__(self, empty_comp):
-#     for key, value in data.items():
-#             setattr(self, key, value)
